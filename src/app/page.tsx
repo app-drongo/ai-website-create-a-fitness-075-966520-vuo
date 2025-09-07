d8f0fe579714889b320e54e5676d82d2a3b31d2d
AI Website Generator <generator@yourplatform.com>
2025-09-07 20:29:30 +0000

    Initial website generation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/sections/Hero'
+import Cta from '@/components/sections/Cta'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Cta />
+    </>
+  )
+}
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navigation } from '@/components/layout/Navigation'
+export { default as Footer } from '@/components/layout/Footer'
+export { default as Hero } from '@/components/sections/Hero'
+export { default as Cta } from '@/components/sections/Cta'
